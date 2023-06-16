20f82d1611102ff2db6d506f9fccdeaf38cbc938
gkovacs81 <gkovacs81@gmail.com>
2023-06-16 20:43:40 +0200

    Fix power on/off

--- src/monitor/monitor.py
+++ src/monitor/monitor.py
@@ -303,11 +303,11 @@ class Monitor(Thread):
 
         if new_power_source == PowerAdapter.SOURCE_BATTERY and self._power_source == PowerAdapter.SOURCE_NETWORK:
             send_power_state(POWER_SOURCE_BATTERY)
-            Notifier.notify_power_outage_started(datetime.now())
+            Notifier.notify_power_outage_started(dt.now())
             self._logger.info("Power outage started!")
         elif new_power_source == PowerAdapter.SOURCE_NETWORK and self._power_source == PowerAdapter.SOURCE_BATTERY:
             send_power_state(POWER_SOURCE_NETWORK)
-            Notifier.notify_power_outage_started(datetime.now())
+            Notifier.notify_power_outage_started(dt.now())
             self._logger.info("Power outage ended!")
 
         self._power_source = new_power_source
