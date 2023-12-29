b8c534a51a3396f82ef37d7fdb3429a775a34e01
gkovacs81 <gkovacs81@gmail.com>
2023-12-29 07:37:17 +0100

    Wifi watchdog with reboot

--- etc/wifi-watchdog.sh
+++ etc/wifi-watchdog.sh
@@ -2,20 +2,38 @@
 
 ## Wifi watchdog script
 # schedule with cron
-# */30 * * * * systemd-cat -t 'argus_wifi' bash -c 'sudo /home/argus/wifi-watchdog.sh'
+# */10 * * * * systemd-cat -t 'argus_wifi' bash -c 'sudo /home/argus/wifi-watchdog.sh'
 ##
 
 SCRIPT_NAME="wifi-watchdog"
 
 WIFI_STATUS_CMD="iwgetid"
 
-WIFI_RESTART_CMD="sudo systemctl restart wpa_supplicant"
+WIFI_RESTART_CMD="sudo systemctl restart networking wpa_supplicant"
+
+WIFI_RESTART_MAX_ATTEMPTS=5
+WIFI_RESTART_ATTEMPT=0
 
 WIFI_STATUS=$(${WIFI_STATUS_CMD} | awk '{print $2}')
 
 if [[ ${WIFI_STATUS} == "" ]]; then
-    echo "No living wifi connection. Restarting wpa supplicant..."
-    ${WIFI_RESTART_CMD}
+    WIFI_STATE_FILE="/tmp/wifi-watchdog.state"
+    if [[ -f ${WIFI_STATE_FILE} ]]; then
+        WIFI_RESTART_ATTEMPT=$(cat ${WIFI_STATE_FILE})
+    fi
+
+    if [[ ${WIFI_RESTART_ATTEMPT} -lt ${WIFI_RESTART_MAX_ATTEMPTS} ]]; then
+        echo "No living wifi connection. Restarting wpa supplicant..."
+        WIFI_RESTART_ATTEMPT=$((WIFI_RESTART_ATTEMPT+1))
+        echo ${WIFI_RESTART_ATTEMPT} > ${WIFI_STATE_FILE}
+        ${WIFI_RESTART_CMD}
+    else
+        echo "Wifi restart failed. Restarting host..."
+        sudo reboot
+    fi
 else
     echo "Wifi connection is OK on $WIFI_STATUS"
+    if [[ -f ${WIFI_STATE_FILE} ]]; then
+        rm ${WIFI_STATE_FILE}
+    fi
 fi
