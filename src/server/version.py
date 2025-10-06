4621c12d4995d9dd5d2a3949d9a026ba753474fd
gkovacs81 <gkovacs81@gmail.com>
2025-10-06 13:07:19 +0200

    Move version to json

--- src/server/version.py
+++ src/server/version.py
@@ -1,1 +1,10 @@
-__version__="v1.4.0_RC05:7c5d088"
+
+import json
+import os
+
+
+version_file = os.path.join(os.path.dirname(__file__), "version.json")
+with open(version_file, "r", encoding="utf-8") as f:
+    version = json.load(f)
+
+__version__=version["version"]
