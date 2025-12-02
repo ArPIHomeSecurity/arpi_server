c1f8a9d77cc6376d73a713bee2d352b72af00b11
gkovacs81 <gkovacs81@gmail.com>
2025-12-02 17:45:27 +0100

    Fix dyndns job

--- src/tools/schedule.py
+++ src/tools/schedule.py
@@ -5,13 +5,6 @@ from crontab import CronTab
 from utils.constants import LOG_IPC
 
 
-dyndns_job = (
-    "systemd-cat -t 'argus_dyndns' "
-    "bash -c 'cd /home/argus/server/;"
-    " PYTHONPATH=/home/argus/server/src /usr/bin/python3"
-    " /home/argus/server/bin/dyndns.py'"
-)
-
 def enable_dyndns_job(enable=True):
     try:
         argus_cron = CronTab(user="argus")
@@ -23,7 +16,7 @@ def enable_dyndns_job(enable=True):
     job = jobs[0] if jobs else None
     if job is None:
         job = argus_cron.new(
-            command=dyndns_job,
+            command="systemd-cat -t 'argus_dyndns' bash -c 'arpi-dyndns'",
             comment="Update the IP address at the dynamic DNS provider",
         )
 
