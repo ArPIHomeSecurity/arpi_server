c23f46926fdc6ba5bcfc750d54f5f5ac458d767f
gkovacs81 <gkovacs81@gmail.com>
2025-12-10 17:19:56 +0100

    Fix mqtt dev start

--- scripts/start_mqtt.sh
+++ scripts/start_mqtt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+CURRENT_PATH=$(dirname "${BASH_SOURCE[0]}")
+
 docker volume create argus-mqtt
 docker start argus-mqtt || docker run -d -it \
     --name argus-mqtt \
     -p 127.0.0.1:1883:1883 \
     -p 127.0.0.1:9001:9001 \
-    -v $(pwd)/etc/mosquitto/mosquitto.dev.conf:/mosquitto/config/mosquitto.conf:ro \
+    -v ${CURRENT_PATH}/mosquitto/mosquitto.dev.conf:/mosquitto/config/mosquitto.conf:ro \
     -v arpi-mosquitto:/mosquitto/data/ \
     eclipse-mosquitto
